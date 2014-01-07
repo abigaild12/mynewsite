64b30536d25968efb8473acec61bb894a6cb3392
abigaild12 <abigail_diaz2002@yahoo.com>
2014-01-06 22:52:09 -0500

    Delete callback.js

--- js/callback.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-  $("button").click(function(){
-     $("ul > li a").replaceWith("<a>~ @i^, 70k ! iii. Ex$ | `~  0Os. </a>");
-     $("ul > li p, p:nth-child(2), p:nth-child(3), p:nth-child(4), p:nth-child(5)").replaceWith("<p>L] [][ ! iii ! o ops ** ! the. </p>");
-     $("ul > li p:nth-child(6)").replaceWith("<p>|||. ||| ! ||| ! ||| !. The @@@ ! ! ! !. ||| ||| ! ||| ! ||| !. The @@@ ! ! ! !.</p>");
-  });
-});
